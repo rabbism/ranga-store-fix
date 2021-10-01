620bc63c2f9e0d2f56530b4a1dd6342e2b8764ad
rabbismgit config --global user.name rabbism <alrabbism@gmailcom>
2021-10-01 20:00:24 +0600

    all done

--- js/app.js
+++ js/app.js
@@ -12,15 +12,17 @@ const showProducts = (products) => {
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
+    div.innerHTML = `<div class="single-product shadow-lg p-3 mb-5 bg-body rounded">
       <div>
     <img class="product-image" src=${image}></img>
       </div>
+    <div class="card-style">
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
+      <p>Ratting: ${product.rating.rate} / <span>${product.rating.count}</span></p>
+      <h2>Price:${product.price} </h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button id="details-btn" class="btn btn-danger">Details</button></div></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
@@ -36,7 +38,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -45,12 +47,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total;
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = Math.round(value,2);
 };
 
 // update delivery charge and total Tax
